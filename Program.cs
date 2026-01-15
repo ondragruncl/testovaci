cb13cf09acfad5ebcbb29e775868ab68c82f4ef2
ogruncl <c24gruncl@oakostelec.cz>
2026-01-15 08:19:03 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp4
             Console.WriteLine("ahoj");
             Console.WriteLine("hello world");
             Console.WriteLine("třetí radek");
+            Console.WriteLine("pátej řádek");
         }
     }
 }
